2433cef88c407a3ce8b0bfcbe16c64251d4ea6ba
nicolamaraschi <nicola.maraschi01@gmail.com>
2023-10-16 21:10:54 +0200

    per ora ho messo 4 bottoni è ho implementato la lista dei composti chimici attraverso fetch api dal db

--- lib/lista_composti_page.dart
+++ lib/lista_composti_page.dart
@@ -20,6 +20,7 @@ class _ListaCompostiPageState extends State<ListaCompostiPage> {
     getComposti();
   }
 
+
   void getComposti() async {
     try {
       final response = await http.get(Uri.parse("http://192.168.1.2:8080/api/listaComposti"));
